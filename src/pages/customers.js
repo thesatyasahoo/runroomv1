ddbd647a9a0016cc3c3c266eeb35eb6cf316d1fb
satya sahoo <satyasahoo525@gmail.com>
2022-12-13 10:11:56 +0530

    code updated

--- src/pages/customers.js
+++ src/pages/customers.js
@@ -1,23 +1,21 @@
-import Head from 'next/head';
-import { Box, Container } from '@mui/material';
-import { CustomerListResults } from '../components/customer/customer-list-results';
-import { CustomerListToolbar } from '../components/customer/customer-list-toolbar';
-import { DashboardLayout } from '../components/dashboard-layout';
-import { customers } from '../__mocks__/customers';
+import Head from "next/head";
+import { Box, Container } from "@mui/material";
+import { CustomerListResults } from "../components/customer/customer-list-results";
+import { CustomerListToolbar } from "../components/customer/customer-list-toolbar";
+import { DashboardLayout } from "../components/dashboard-layout";
+import { customers } from "../__mocks__/customers";
 import { AuthGuard } from "../contexts/auth-guard";
 
 const Page = () => (
   <>
     <Head>
-      <title>
-        Customers | RunRoom
-      </title>
+      <title>Customers</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth={false}>
--- src/pages/runroom.js
+++ src/pages/runroom.js
@@ -1,27 +1,25 @@
-import Head from 'next/head';
-import { Box, Container } from '@mui/material';
-import { RunroomCreate } from '../components/runroom/runroom-create';
-import { RunroomCreateToolbar } from '../components/runroom/runroom-create-toolbar';
-import { DashboardLayout } from '../components/dashboard-layout';
-import { customers } from '../__mocks__/customers';
+import Head from "next/head";
+import { Box, Container } from "@mui/material";
+import { RunroomCreate } from "../components/runroom/runroom-create";
+import { RunroomCreateToolbar } from "../components/runroom/runroom-create-toolbar";
+import { DashboardLayout } from "../components/dashboard-layout";
+import { customers } from "../__mocks__/customers";
 import { AuthGuard } from "../contexts/auth-guard";
 
 const Page = () => (
   <>
     <Head>
-      <title>
-        Runroom | RunRoom
-      </title>
+      <title>Runroom</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth={false}>
-        <RunroomCreateToolbar/>
+        <RunroomCreateToolbar />
         <Box sx={{ mt: 3 }}>
           <RunroomCreate runrooms={customers} />
         </Box>
