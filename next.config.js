dcc27681041a01a07316e8fc7fb13da12b225307
satya sahoo <satyasahoo525@gmail.com>
2022-12-15 19:51:57 +0530

    added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// next.config.js
+
 module.exports = {
-  reactStrictMode: true
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(
+      new webpack.IgnorePlugin({ resourceRegExp: /\.\/native/, contextRegExp: /\/pg\// })
+    );
+
+    // Important: return the modified config
+    return config;
+  },
 };
